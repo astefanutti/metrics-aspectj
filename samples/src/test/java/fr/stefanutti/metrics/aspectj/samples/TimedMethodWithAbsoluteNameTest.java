22878cd70c028475429c261859b396ea9b441477
Antonin Stefanutti <antonin@stefanutti.fr>
2013-09-25 12:59:56 -0700

    Rename Maven modules

---
+++ samples/src/test/java/fr/stefanutti/metrics/aspectj/samples/TimedMethodWithAbsoluteNameTest.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (C) 2013 Antonin Stefanutti (antonin.stefanutti@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.stefanutti.metrics.aspectj.samples;
+
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.SharedMetricRegistries;
+import com.codahale.metrics.Timer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class TimedMethodWithAbsoluteNameTest {
+
+    private final static String REGISTRY_NAME = "timerRegistryForAbsoluteNamedTimer";
+
+    private final static String TIMER_NAME = "absolutelyTimedMethod";
+
+    private TimedMethodWithAbsoluteName instance;
+
+    @Before
+    public void createTimedInstance() {
+        instance = new TimedMethodWithAbsoluteName();
+    }
+
+    @After
+    public void clearSharedMetricRegistries() {
+        SharedMetricRegistries.clear();
+    }
+
+    @Test
+    public void timedMethodNotCalledYet() {
+        assertThat(SharedMetricRegistries.names(), hasItem(REGISTRY_NAME));
+        MetricRegistry registry = SharedMetricRegistries.getOrCreate(REGISTRY_NAME);
+        assertThat(registry.getTimers(), hasKey(TIMER_NAME));
+        Timer timer = registry.getTimers().get(TIMER_NAME);
+
+        // Make sure that the timer hasn't been called yet
+        assertThat(timer.getCount(), is(equalTo(0L)));
+    }
+
+    @Test
+    public void callTimedMethodOnce() {
+        assertThat(SharedMetricRegistries.names(), hasItem(REGISTRY_NAME));
+        MetricRegistry registry = SharedMetricRegistries.getOrCreate(REGISTRY_NAME);
+        assertThat(registry.getTimers(), hasKey(TIMER_NAME));
+        Timer timer = registry.getTimers().get(TIMER_NAME);
+
+        // Call the timed method and assert it's been timed
+        instance.absolutelyTimedMethod();
+        assertThat(timer.getCount(), is(equalTo(1L)));
+    }
+}
