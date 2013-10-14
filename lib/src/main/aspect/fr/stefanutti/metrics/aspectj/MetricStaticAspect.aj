0df15f52e4bdb91410c60d368c42bbcbeb1542ab
Antonin Stefanutti <antonin@stefanutti.fr>
2013-10-14 22:05:03 +0200

    Add support of @Metered for static methods

--- lib/src/main/aspect/fr/stefanutti/metrics/aspectj/MetricStaticAspect.aj
+++ lib/src/main/aspect/fr/stefanutti/metrics/aspectj/MetricStaticAspect.aj
@@ -42,6 +42,13 @@ final aspect MetricStaticAspect extends AbstractMetricAspect {
         final MetricStrategy strategy = MetricStrategyFactory.newInstance(clazz);
         for (final Method method : clazz.getDeclaredMethods()) {
             if (Modifier.isStatic(method.getModifiers()) && !method.isSynthetic()) {
+                metricAnnotation(clazz, method, strategy, Metered.class, new MetricFactory() {
+                    @Override
+                    public Metric metric(MetricRegistry registry, String name, boolean absolute) {
+                        String finalName = name.isEmpty() ? method.getName() : strategy.resolveMetricName(name);
+                        return registry.meter(absolute ? finalName : MetricRegistry.name(clazz, finalName));
+                    }
+                });
                 metricAnnotation(clazz, method, strategy, Timed.class, new MetricFactory() {
                     @Override
                     public Metric metric(MetricRegistry registry, String name, boolean absolute) {
---
+++ samples/se/src/test/java/fr/stefanutti/metrics/aspectj/samples/se/MeteredStaticMethodWithRegistryFromStringTest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2013 Antonin Stefanutti (antonin.stefanutti@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.stefanutti.metrics.aspectj.samples.se;
+
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.SharedMetricRegistries;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class MeteredStaticMethodWithRegistryFromStringTest {
+
+    private final static String REGISTRY_NAME = "singleMeterStaticRegistry";
+
+    private final static String METER_NAME = MeteredStaticMethodWithRegistryFromString.class.getName() + "." + "singleMeteredStaticMethod";
+
+    @Test
+    public void callMeteredStaticMethodOnce() {
+        // Call the metered method and assert it's been marked
+        MeteredStaticMethodWithRegistryFromString.singleMeteredStaticMethod();
+
+        assertThat("Shared metric registry is not created", SharedMetricRegistries.names(), hasItem(REGISTRY_NAME));
+        MetricRegistry registry = SharedMetricRegistries.getOrCreate(REGISTRY_NAME);
+        assertThat("Timer is not registered", registry.getMeters(), hasKey(METER_NAME));
+        Meter meter = registry.getMeters().get(METER_NAME);
+        assertThat("Meter count is incorrect", meter.getCount(), is(equalTo(1L)));
+    }
+}
